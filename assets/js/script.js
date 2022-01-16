15a69afb9cc648f52f4eadeb2cbb2485300c5546
Héctor <hpma.sence@gmail.com>
2022-01-16 20:12:53 -0300

    se carga carpeta assets

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+$(function () {
+  $("#enviarCorreo").click(function() { 
+      alert("¡El correo ha sido enviado exitosamene!")
+  })
+  
+  
+  
+  $(".card-text").click(function () {
+      $(".card-text").toggle();
+  
+  
+  })
+  
+  
+  $('body').on('dblclick','h4',function() {
+  $(this).css("color","red");
+  
+  
+  
+  })
+  
+  
+  })
+
+
+
+$(document).ready(function(){
+
+
+$("a").click(function (){
+var gato= this.hash
+/*alert(gato)  ejemplo de alert navx y scroll*/
+
+$("html, body").animate(
+    {
+        scrolltop: $(gato).offset().top 
+      },
+        800
+
+)
+
+
+
+
+})
+
+})
+
+
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
